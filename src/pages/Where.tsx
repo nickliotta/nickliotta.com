497939b85152fa3b7f959f53b9ae180ed81318a4
Nicholas Liotta <nicholas.liotta.edu@gmail.com>
2025-09-28 18:32:37 -0400

    Automated experiences based on current and previous roles

--- src/pages/Where.tsx
+++ src/pages/Where.tsx
@@ -9,6 +9,111 @@ import GladstoneInstitutes from "../assets/images/gladstone-institutes.png";
 import { Helmet } from "react-helmet";
 
 const Where = () => {
+    const experiences = [
+        {
+            logo                    : Northwell,
+            timeline                : "May 2024 - Present",
+            company                 : "Northwell Health",
+            department              : "Feinstein Institutes for Medical Research",
+            location                : "Manhasset, New York",
+            principalInvestigator   : { name: "Dr. Douglas Nixon", url: "https://feinstein.northwell.edu/institutes-researchers/our-researchers/douglas-f-nixon-md-phd" },
+            advisors                : "Advised by Helena Reyes-Gopar, Nicholas Dopkins, and Douglas Nixon",
+            role                    : "Consultant Bioinformatician"
+        },
+        {
+            logo                    : ColdSpringHarborLaboratory,
+            timeline                : "January 2022 - Present",
+            company                 : "Cold Spring Harbor Laboratory",
+            department              : "Science education",
+            location                : "Cold Spring Harbor, New York",
+            role                    : "Head College Intern"
+        },
+        {
+            logo                    : CarnegieMellonUniversity,
+            timeline                : "May 2025 - August 2025",
+            company                 : "Carnegie Mellon University",
+            department              : "Department of Psychology",
+            location                : "Pittsburgh, Pennsylvania",
+            principalInvestigator   : { name: "Dr. Jonathan Tsay", url: "https://www.tsaylab.com/" },
+            advisors                : "Advised by Jonathan Tsay",
+            role                    : "Undergraduate Research Fellow"
+        },
+        {
+            logo                    : WeillCornell,
+            timeline                : "February 2022 - April 2024",
+            company                 : "Weill Cornell Medicine",
+            department              : "Department of Medicine",
+            location                : "New York, New York",
+            principalInvestigator   : { name: "Dr. Douglas Nixon", url: "https://vivo.weill.cornell.edu/display/cwid-dnixon" },
+            advisors                : "Advised by Bhavya Singh, Jez L. Marston, and Douglas Nixon",
+            role                    : "Undergraduate Research Assistant"
+        },
+        {
+            logo                    : GladstoneInstitutes,
+            timeline                : "January 2022 - August 2022",
+            company                 : "University of California, San Francisco",
+            department              : "Gladstone Institute",
+            location                : "San Francisco, California",
+            principalInvestigator   : { name: "Dr. Nevan Krogan", url: "https://kroganlab.ucsf.edu/krogan-lab" },
+            advisors                : "Advised by Mehdi Bouhaddou",
+            role                    : "High School Student Researcher"
+        }
+    ];
+
+    const current = experiences.filter(experience => experience.timeline.includes("Present"));
+    const previous = experiences.filter(experience => !experience.timeline.includes("Present"));
+
+    type WorkExperience = {
+        logo: string;
+        timeline: string;
+        company: string;
+        department: string;
+        location: string;
+        principalInvestigator?: { name: string; url: string };
+        advisors?: string;
+        role: string;
+    };
+
+    const renderWorkItems = (workList: WorkExperience[]) => {
+        return workList.map((work: WorkExperience) => (
+            <Work
+                logo                    = {work.logo}
+                timeline                = {work.timeline}
+                company                 = {work.company}
+                department              = {work.department}
+                location                = {work.location}
+                principalInvestigator   = {work.principalInvestigator}
+                advisors                = {work.advisors}
+                role                    = {work.role}
+            />
+        ));
+    };
+
+
+    const renderCurrentWork = () => {
+        if (current.length === 0) return <></>;
+        return (
+            <>
+                <h3>Currently</h3>
+                <WorkWrapper>
+                    {renderWorkItems(current)}
+                </WorkWrapper>
+            </>
+        );
+    };
+
+    const renderPreviousWork = () => {
+        if (previous.length === 0) return <></>;
+        return (
+            <>
+                <h3>Previously</h3>
+                <WorkWrapper>
+                    {renderWorkItems(previous)}
+                </WorkWrapper>
+            </>
+        );
+    };
+
     return (
         <PageWrapper>
             <Helmet>
@@ -16,62 +121,8 @@ const Where = () => {
             </Helmet>
 
             <h1>Where I am</h1>
-      
-            <h3>Currently</h3>
-            <WorkWrapper>
-                <Work
-                    logo                   = {Northwell}
-                    timeline               = "May 2024 - Present"
-                    company                = "Northwell Health"
-                    department             = "Feinstein Institutes for Medical Research"
-                    location               = "Manhasset, New York"
-                    principalInvestigator  = {{ name: "Dr. Douglas Nixon", url: "https://feinstein.northwell.edu/institutes-researchers/our-researchers/douglas-f-nixon-md-phd" }}
-                    advisors               = "Advised by Helena Reyes-Gopar, Nicholas Dopkins, and Douglas Nixon"
-                    role                   = "Consultant Bioinformatician"
-                />
-                <Work
-                    logo                   = {ColdSpringHarborLaboratory}
-                    timeline               = "January 2022 - Present"
-                    company                = "Cold Spring Harbor Laboratory"
-                    department             = "Science education"
-                    location               = "Cold Spring Harbor, New York"
-                    role                   = "Head College Intern"
-                />
-            </WorkWrapper>
-
-            <h3>Previously</h3>
-            <WorkWrapper>
-                <Work
-                    logo                   = {CarnegieMellonUniversity}
-                    timeline               = "May 2025 - August 2025"
-                    company                = "Carnegie Mellon University"
-                    department             = "Department of Psychology"
-                    location               = "Pittsburgh, Pennsylvania"
-                    principalInvestigator  = {{ name: "Dr. Jonathan Tsay", url: "https://www.tsaylab.com/" }}
-                    advisors               = "Advised by Jonathan Tsay"
-                    role                   = "Undergraduate Research Fellow"
-                />
-                <Work
-                    logo                   = {WeillCornell}
-                    timeline               = "February 2022 - April 2024"
-                    company                = "Weill Cornell Medicine"
-                    department             = "Department of Medicine"
-                    location               = "New York, New York"
-                    principalInvestigator  = {{ name: "Dr. Douglas Nixon", url: "https://vivo.weill.cornell.edu/display/cwid-dnixon" }}
-                    advisors               = "Advised by Bhavya Singh, Jez L. Marston, and Douglas Nixon"
-                    role                   = "Undergraduate Research Assistant"
-                />
-                <Work
-                    logo                   = {GladstoneInstitutes}
-                    timeline               = "January 2022 - August 2022"
-                    company                = "University of California, San Francisco"
-                    department             = "Gladstone Institute"
-                    location               = "San Francisco, California"
-                    principalInvestigator  = {{ name: "Dr. Nevan Krogan", url: "https://kroganlab.ucsf.edu/krogan-lab" }}
-                    advisors               = "Advised by Mehdi Bouhaddou"
-                    role                   = "High School Student Researcher"
-                />
-            </WorkWrapper>
+            {renderCurrentWork()}
+            {renderPreviousWork()}
         </PageWrapper>
     );
 };

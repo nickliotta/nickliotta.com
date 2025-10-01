beee112a9095a49971334ed076c98133459ab353
Nicholas Liotta <nicholas.liotta.edu@gmail.com>
2025-10-01 18:15:41 -0400

    Fix pages crashing

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,10 @@ import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+    plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+    build: {
+        rollupOptions: {
+            external: ["#minpath"], 
+        },
+    },
 });
